c330bd06d7ff08bf9da1e4649f3701015367b853
Nathaniel Rubin <nateboop@gmail.com>
2018-07-01 22:06:40 -0400

    Fuller deploy func

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
 
+cd $PROJECT_DIR
+git fetch origin && git reset --hard origin/master
+
+docker_id=$(docker ps -aqf "name=portfolio_site")
+docker exec $docker_id bundle check
+can_restart=$?
+
+docker build -t portfolio_site .
+
 find . -type f -name '*.scss'  -exec cat {} + >> styles.scss
 bundle exec sass styles.scss > public/css/styles.css
+
+if [ $can_restart -eq 0 ] then
+  docker restart $docker_id
+else
+  docker stop $docker_id
+  docker rm $docker_id
+  docker run --name portfolio_site -e "VIRTUAL_HOST=$VIRTUAL_HOST" -e "LETSENCRYPT_HOST=$LETSENCRYPT_HOST"  -e "LETSENCRYPT_EMAIL=$LETSENCRYPT_EMAIL" -v $PROJECT_DIR:/app -d portfolio_site
+fi
